intel/u-nit

commit e01e5c55ff4bbbf4e175b3694055fd5c9b17be6b

pid 1: Perform userspace setup - and teardown for shutdown/reboot
init is now ready to work as PID 1 - the process to which Linux kernel
handles the task of setting up userspace. As so, init does not accept
inittab file as argument anymore - instead, reads inittab from a default
location `/etc/inittab2` - which can be changed via INITTAB_FILENAME
parameter.

On start, init performs userspace setup tasks. Note that mouting
filesystems is still incomplete: it only mounts system ones, like
`/proc` or `/sys`. Mounting other filesystems will be done later -
probably by reading `/etc/fstab` file.

init also accepts signals `SIGTERM`, `SIGUSR1` and `SIGUSR2` to reboot,
halt and poweroff system, respectively. No other signal is accepted.

Ctrl+Alt+Del and sysrq keys are also disabled - although nothing
prevents sysrq keys from being re-enabled again by user.

On shutdown/reboot, init closes all mounted filesystems which are not of
system. It does so in a way that filesystems on top of others are
unmounted first.

Note that at this moment, init does not execute getty: to have a
console, one needs to call getty as service on inittab.

Requirement: 251159
Requirement: 251160
Requirement: 251162
Requirement: 251166
Requirement: 251168
Requirement: 251169
Requirement: 251174
Requirement: 251175

Reviewed-by: Flavio Ceolin <flavio.ceolin@intel.com>

src/main.c
@@ -1,19 +1,26 @@
 #include <assert.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/reboot.h>
+#include <sys/signalfd.h>
+#include <sys/stat.h>
+#include <sys/ttydefaults.h> /* CKILL, CINTR, CQUIT, ... */
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
-#include <sys/ioctl.h>
-#include <sys/signalfd.h>
+#include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "inittab.h"
 #include "log.h"
 #include "mainloop.h"
+#include "mount.h"
 
 #ifndef TIMEOUT_TERM
 #define TIMEOUT_TERM 3000
@@ -21,6 +28,10 @@
 
 #ifndef TIMEOUT_ONE_SHOT
 #define TIMEOUT_ONE_SHOT 3000
+#endif
+
+#ifndef INITTAB_FILENAME
+#define INITTAB_FILENAME "/etc/inittab2"
 #endif
 
 enum stage {
@@ -54,15 +65,7 @@ static enum stage current_stage;
 static struct mainloop_timeout *kill_timeout;
 static struct mainloop_timeout *one_shot_timeout;
 
-static void
-usage(const char *invocation_name)
-{
-    log_message("Usage: \n"
-            " %s <inittab-file>\n"
-            "\n"
-            "System and service manager [early development]\n",
-            invocation_name);
-}
+static int shutdown_command = RB_AUTOBOOT; /* Is this a sensible default? */
 
 static void
 remove_process(struct process **list, struct process *p)
@@ -105,13 +108,13 @@ static int
 run_exec(const char *command)
 {
     /* should run the new process using a bash ? */
-//    int r;
+    int r;
 
     /* Give a controlle terminal for the process */
-//    r = ioctl(STDIN_FILENO, TIOCSCTTY, 1);
-//    if (r == -1) {
-//        return r;
-//    }
+    r = ioctl(STDIN_FILENO, TIOCSCTTY, 1);
+    if (r == -1) {
+        return r;
+    }
 
     /* Should configure ENV ? */
     return execl("/bin/sh", "/bin/sh", "-c", command, NULL);
@@ -123,8 +126,10 @@ setup_signals(sigset_t *mask)
     int i, r;
 
     int signals[] = {
-        SIGCHLD,
-        SIGINT
+        SIGCHLD,    /* To monitor started processes */
+        SIGTERM,    /* Reboot signal */
+        SIGUSR1,    /* Halt signal */
+        SIGUSR2     /* Shutdown signal */
     };
 
     r = sigemptyset(mask);
@@ -137,6 +142,136 @@ setup_signals(sigset_t *mask)
 
     r = sigprocmask(SIG_SETMASK, mask, NULL);
     assert(r == 0);
+}
+
+/* Try to open the /dev/console, the maximum attempts is 10 times with
+ * 100000 microseconds between each attempt
+ */
+static int
+open_console(const char *terminal, int mode)
+{
+    int times = 10;
+    int tty = -1;
+
+    for (; times > 0; times--) {
+        struct timespec pause = {};
+
+        errno = 0;
+        tty = open(terminal, mode);
+        if (tty >= 0) {
+            break;
+        }
+
+        if (errno != EIO) {
+            break;
+        }
+
+        /* Isn't this a really, really long time? */
+        pause.tv_nsec = 100 * 1000 * 1000; // 100 msecs
+        (void)nanosleep(&pause, NULL);
+    }
+
+    return tty;
+}
+
+static bool
+reset_console(int fd)
+{
+    int r;
+    struct termios tty;
+    bool result = false;
+
+    r = tcgetattr(fd, &tty);
+    if (r == -1) {
+        goto end;
+    }
+
+    /* TODO assess what is really relevant to our case */
+    tty.c_cflag &= CBAUD|CBAUDEX|CSIZE|CSTOPB|PARENB|PARODD;
+    tty.c_cflag |= HUPCL|CLOCAL|CREAD;
+    tty.c_iflag = IGNPAR|ICRNL|IXON|IXANY;
+    tty.c_oflag = OPOST|ONLCR;
+    tty.c_lflag = ISIG|ICANON|ECHO|ECHOCTL|ECHOPRT|ECHOKE;
+
+    tty.c_cc[VINTR]     = CINTR;            /* ^C */
+    tty.c_cc[VQUIT]     = CQUIT;            /* ^\ */
+    tty.c_cc[VERASE]    = CERASE;           /* ASCII DEL (0177) */
+    tty.c_cc[VKILL]     = CKILL;            /* ^X */
+    tty.c_cc[VEOF]      = CEOF;             /* ^D */
+    tty.c_cc[VTIME]     = 0;
+    tty.c_cc[VMIN]      = 1;
+    tty.c_cc[VSTART]    = CSTART;           /* ^Q */
+    tty.c_cc[VSTOP]     = CSTOP;            /* ^S */
+    tty.c_cc[VSUSP]     = CSUSP;            /* ^Z */
+    tty.c_cc[VEOL]      = _POSIX_VDISABLE;
+    tty.c_cc[VREPRINT]  = CREPRINT;         /* ^R */
+    tty.c_cc[VWERASE]   = CWERASE;          /* ^W */
+    tty.c_cc[VLNEXT]    = CLNEXT;           /* ^V */
+    tty.c_cc[VEOL2]     = _POSIX_VDISABLE;
+
+    /*
+     * Now set the terminal line.
+     * We don't care about non-transmitted output data
+     * and non-read input data.
+     */
+    r = tcsetattr(fd, TCSANOW, &tty);
+    if (r == -1) {
+        goto end;
+    }
+
+    r = tcflush(fd, TCIOFLUSH);
+    if (r == -1) {
+        goto end;
+    }
+
+    /* Everything went OK */
+    result = true;
+
+end:
+    return result;
+}
+
+static int
+setup_stty(const char *terminal)
+{
+    int r, tty;
+
+    tty = open_console(terminal, O_RDWR | O_NOCTTY | O_CLOEXEC);
+    if (tty == -1) {
+        goto err_open;
+    }
+
+    r = dup2(tty, STDIN_FILENO);
+    if (r == -1) {
+        goto err_dup_in;
+    }
+
+    r = dup2(tty, STDOUT_FILENO);
+    if (r == -1) {
+        goto err_dup_out;
+    }
+
+    r = dup2(tty, STDERR_FILENO);
+    if (r == -1) {
+        goto err_dup_errno;
+    }
+
+    if (!reset_console(tty)) {
+        goto err_console;
+    }
+
+    return tty;
+
+err_console:
+    (void)close(STDERR_FILENO);
+err_dup_errno:
+    (void)close(STDOUT_FILENO);
+err_dup_out:
+    (void)close(STDIN_FILENO);
+err_dup_in:
+    (void)close(tty);
+err_open:
+    return -1;
 }
 
 /* Expects SIGCHLD to be disabled when called */
@@ -152,13 +287,10 @@ spawn_exec(const char *command, const char *console)
     log_message("fork result for '%s': %d\n", command, p);
     /* the caller is reponsible to check the error */
     if (p != 0) {
-        /* Restore the signals */
-//        r = sigprocmask(SIG_SETMASK, &old_mask, NULL);
         return p;
     }
 
     /* child code */
-
     r = sigemptyset(&mask);
     assert(r == 0);
 
@@ -172,10 +304,9 @@ spawn_exec(const char *command, const char *console)
     }
 
     /* Configure terminal for child */
-//    r = setup_stty(console);
-//    if (r == -1) {
-//        return -1;
-//    }
+    if (setup_stty(console) < 0) {
+        return -1;
+    }
 
     return run_exec(command);
 }
@@ -214,7 +345,8 @@ start_processes(struct inittab_entry *list)
                 result = false;
                 break;
             }
-            p->pid = spawn_exec(entry->process_name, "");
+            /* Always /dev/console? Do we always need this? */
+            p->pid = spawn_exec(entry->process_name, "/dev/console");
 
             if (p->pid > 0) {
                 /* Stores inittab entry information on process struct */
@@ -350,8 +482,10 @@ term_running_process(void)
 }
 
 static void
-handle_sigint(struct signalfd_siginfo *info)
+handle_shutdown_cmd(struct signalfd_siginfo *info, int command)
 {
+    (void)info; /* Not used */
+
     /* Ensure 'remaining list' is cleaned up */
     remaining.remaining = NULL;
     remaining.pending_finish = 0;
@@ -360,6 +494,11 @@ handle_sigint(struct signalfd_siginfo *info)
     /* TODO is this right? */
     current_stage = STAGE_TERMINATION;
     term_running_process();
+
+    shutdown_command = command;
+
+    /* Stages will change again, let's keep track */
+    mainloop_set_post_iteration_callback(stage_maintenance);
 }
 
 static void
@@ -404,19 +543,89 @@ handle_child_exit(struct signalfd_siginfo *info)
 static void
 signal_handler(struct signalfd_siginfo *info)
 {
-    log_message("ssi_signo: %d - ssi_code: %d - ssi_pid: %d - ssi_status %d\n",
+    log_message("Received signal - si_signo: %d - ssi_code: %d - ssi_pid: %d - ssi_status %d\n",
             info->ssi_signo, info->ssi_code, info->ssi_pid, info->ssi_status);
 
     switch (info->ssi_signo) {
     case SIGCHLD:
         handle_child_exit(info);
         break;
-    case SIGINT:
-        handle_sigint(info);
+    case SIGTERM:
+        handle_shutdown_cmd(info, RB_AUTOBOOT);
+        break;
+    case SIGUSR1:
+        handle_shutdown_cmd(info, RB_HALT_SYSTEM);
+        break;
+    case SIGUSR2:
+        handle_shutdown_cmd(info, RB_POWER_OFF);
         break;
     default:
+        /* Nothing to do*/
         break;
     }
+}
+
+static bool
+do_reboot(int cmd)
+{
+    bool result = true;
+
+    /* Umount fs */
+    sync(); /* Ensure fs are synced */
+    mount_umount_filesystems();
+
+    if (reboot(cmd) < 0) {
+        log_message("Reboot command failed: %m\n");
+        result = false;
+    }
+
+    return result;
+}
+
+static bool
+disable_sysrq(void)
+{
+    FILE *f;
+    bool result = true;
+
+    errno = 0;
+    f = fopen("/proc/sys/kernel/sysrq", "r+e");
+    if (f == NULL) {
+        log_message("Could not open sysrq file: %m\n");
+        result = false;
+        goto end;
+    }
+
+    errno = 0;
+    if (fputc((int)'0', f) == EOF) {
+        log_message("Could not write to sysrq file: %m\n");
+        result = false;
+    }
+
+    (void)fclose(f);
+
+end:
+    return result;
+}
+
+static bool
+setup_console(void)
+{
+    int tty;
+    bool r = false;
+
+    /* Closing stdio descriptors */
+    close(STDIN_FILENO);
+    close(STDERR_FILENO);
+    close(STDOUT_FILENO);
+
+    tty = open_console("/dev/console", O_WRONLY | O_NOCTTY | O_CLOEXEC);
+    if (tty != -1) {
+        r = reset_console(tty);
+        close(tty); /* Why this close here?*/
+    }
+
+    return r;
 }
 
 int
@@ -424,17 +633,43 @@ main(int argc, char *argv[])
 {
     sigset_t mask;
     struct mainloop_signal_handler *msh = NULL;
-    int result = EXIT_SUCCESS;
+    int r, result = EXIT_SUCCESS;
+    pid_t p;
 
     current_stage = STAGE_SETUP;
 
-    /* TODO this arg stuff will change once init starts as PID 1 */
-    if (argc != 2) {
-        usage(argv[0]);
+    if (getpid() != 1) {
+        result = EXIT_FAILURE;
         goto end;
     }
 
-    if (!read_inittab(argv[1], &inittab_entries)) {
+    if (!read_inittab(INITTAB_FILENAME, &inittab_entries)) {
+        result = EXIT_FAILURE;
+        goto end;
+    }
+
+    (void)umask(0);
+
+    /* Ensure init will not block any umount call later */
+    r = chdir("/");
+    if (r == -1) {
+        result = EXIT_FAILURE;
+        goto end;
+    }
+
+    if (!setup_console()) {
+        result = EXIT_FAILURE;
+        goto end;
+    }
+
+    /* Become a session leader */
+    p = setsid();
+    if (p == -1) {
+        result = EXIT_FAILURE;
+        goto end;
+    }
+
+    if (!mount_mount_filesystems()) {
         result = EXIT_FAILURE;
         goto end;
     }
@@ -442,8 +677,21 @@ main(int argc, char *argv[])
     /* Block signals that should only be caught by epoll */
     setup_signals(&mask);
 
+    /* To catch Ctrl+alt+del. We will receive SIGINT on Ctrl+alt+del (which we ignore) */
+    r = reboot(RB_DISABLE_CAD);
+    if (r < 0) {
+        log_message("Could not disable Ctrl+Alt+Del: %m\n");
+        /* How big of a problem is this? Should we abort or life goes on? */
+    }
+
+    /* Disable sysrq */
+    if (!disable_sysrq()) {
+        log_message("Coud not disable Sysrq keys\n");
+    }
+
     /* Prepares mainloop to run */
     if (!mainloop_setup()) {
+        result = EXIT_FAILURE;
         goto end;
     }
 
@@ -453,6 +701,7 @@ main(int argc, char *argv[])
 
     msh = mainloop_add_signal_handler(&mask, signal_handler);
     if (msh == NULL) {
+        result = EXIT_FAILURE;
         goto end;
     }
 
@@ -461,6 +710,11 @@ main(int argc, char *argv[])
     start_processes(inittab_entries.startup_list);
 
     mainloop_start();
+
+    if (!do_reboot(shutdown_command)) {
+        result = EXIT_FAILURE;
+        goto end;
+    }
 
 end:
 

Makefile
@@ -17,7 +17,8 @@ SOURCE = \
 	src/lexer.c \
 	src/log.c \
 	src/main.c \
-	src/mainloop.c
+	src/mainloop.c \
+	src/mount.c
 
 OBJS = $(SOURCE:.c=.o)
 

src/mount.c
@@ -1,0 +1,210 @@
+#include "mount.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "log.h"
+
+static const struct mount_table {
+    const char *source;
+    const char *target;
+    const char *fstype;
+    const char *options;
+    unsigned long flags;
+    bool fatal;
+} mount_table[] = {
+    { NULL, "/sys", "sysfs", NULL, MS_NOSUID | MS_NOEXEC | MS_NODEV, true },
+    { NULL, "/proc", "proc", NULL, MS_NOSUID | MS_NOEXEC | MS_NODEV, true },
+    { NULL, "/dev", "devtmpfs", "mode=0755", MS_NOSUID | MS_STRICTATIME, true },
+    { NULL, "/dev/pts", "devpts", "mode=0620", MS_NOSUID | MS_NOEXEC, true },
+    { NULL, "/dev/shm", "tmpfs", "mode=1777", MS_NOSUID | MS_NODEV | MS_STRICTATIME, true },
+    { NULL, "/run", "tmpfs", "mode=0755", MS_NOSUID | MS_NODEV | MS_STRICTATIME, true },
+    { NULL, "/tmp", "tmpfs", NULL, 0, true },
+    { NULL, "/sys/kernel/debug", "debugfs", NULL, 0, false },
+    { NULL, "/sys/kernel/security", "securityfs", NULL, MS_NOSUID | MS_NOEXEC | MS_NODEV, false },
+};
+
+struct mount_point {
+    struct mount_point *next;
+    char *path;
+};
+
+bool
+mount_mount_filesystems(void)
+{
+    const struct mount_table *mnt;
+    bool result = true;
+
+    for (mnt = mount_table;
+            mnt < mount_table + sizeof(mount_table) / sizeof((mount_table)[0]);
+            mnt++) {
+        const char *source = (mnt->source != NULL) ? mnt->source : "none";
+        int err;
+
+        err = mkdir(mnt->target, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+        if (err < 0) {
+            if ((errno != EEXIST) && mnt->fatal) {
+                log_message("could not mkdir '%s': %m\n", mnt->target);
+                result = false;
+                goto end;
+            }
+        }
+
+        log_message("mounting '%s' from '%s' to '%s', options=%s\n",
+                mnt->fstype, source, mnt->target,
+                (mnt->options != NULL) ? mnt->options : "(none)");
+        err = mount(source, mnt->target, mnt->fstype, mnt->flags, mnt->options);
+        if (err < 0) {
+            if (errno == EBUSY || !mnt->fatal) {
+                log_message("could not mount '%s' from '%s' to '%s', options=%s: %m\n",
+                        mnt->fstype, source, mnt->target,
+                        (mnt->options != NULL) ? mnt->options : "(none)");
+            } else {
+                log_message("could not mount '%s' from '%s' to '%s', options=%s: %m\n",
+                        mnt->fstype, source, mnt->target,
+                        (mnt->options != NULL) ? mnt->options : "(none)");
+                result = false;
+                goto end;
+            }
+        }
+    }
+
+end:
+    return result;
+}
+
+static struct mount_point *
+get_mountpoints(void)
+{
+    FILE *mounts_file;
+    struct mount_point *list = NULL;
+
+    mounts_file = fopen("/proc/self/mountinfo", "re");
+    if (mounts_file == NULL) {
+        log_message("Could not open mountinfo file: %m\n");
+        goto error_open;
+    }
+
+    while (true) {
+        const struct mount_table *mnt;
+        struct mount_point *entry;
+        bool should_umount = true;
+        char *path;
+        int r;
+
+        r = fscanf(mounts_file,
+                "%*s "      /* mount id */
+                "%*s "      /* parent id*/
+                "%*s "      /* major: minor*/
+                "%*s "      /* root */
+                "%ms "      /* mount point path. This is what we want */
+                "%*[^\n]",  /* Discard everything else */
+                &path);
+        if (r != 1) {
+            if (r == EOF) {
+                break;
+            }
+            continue;
+        }
+
+        /* No need to umount these 'system' mountpoints */
+        for (mnt = mount_table;
+                mnt < (mount_table + (sizeof(mount_table) / sizeof((mount_table)[0])));
+                mnt++) {
+            if (strcmp(path, mnt->target) == 0) {
+                should_umount = false;
+                break;
+            }
+        }
+
+        if (!should_umount) {
+            free(path);
+            continue;
+        }
+
+        errno = 0;
+        entry = calloc(1, sizeof(struct mount_point));
+        if (entry == NULL) {
+            log_message("Could not create mount point entry: %m\n");
+            free(path);
+            goto error_entry;
+        }
+
+        entry->path = path;
+        entry->next = list;
+        list = entry;
+    }
+
+error_entry:
+    fclose(mounts_file);
+error_open:
+    return list;
+}
+
+static void
+remove_mountpoint(struct mount_point **list, struct mount_point *mp)
+{
+    assert(mp);
+    assert(list);
+
+    if (*list == mp) {
+        *list = mp->next;
+    } else {
+        struct mount_point *current = *list;
+
+        while (current != NULL) {
+            if (current->next == mp) {
+                current->next = mp->next;
+                break;
+            }
+            current = current->next;
+        }
+    }
+
+    free(mp->path);
+    free(mp);
+}
+
+void
+mount_umount_filesystems(void)
+{
+    struct mount_point *mp, *mp_list;
+    bool changed;
+
+    mp_list = get_mountpoints();
+    changed = false;
+    do {
+        /* We keep umounting filesystems as long as we can. Some filesystems
+         * maybe mounted on top of others, so the 'bottom' ones will only umount
+         * when there's nothing left on top of them. After one iteration without
+         * being able to umount anything, we give up. */
+        mp = mp_list;
+        changed = false;
+        while (mp != NULL) {
+            int err;
+            struct mount_point *next = mp->next;
+
+            log_message("Umounting %s\n", mp->path);
+
+            errno = 0;
+            err = umount(mp->path);
+            if (err == 0) {
+                log_message("Umounted %s\n", mp->path);
+                changed = true;
+                remove_mountpoint(&mp_list, mp);
+            } else {
+                log_message("Could not umount: %s: %m\n", mp->path);
+            }
+            mp = next;
+        }
+    } while (changed);
+
+    while (mp_list != NULL) {
+        remove_mountpoint(&mp_list, mp_list);
+    }
+}

src/mount.h
@@ -1,0 +1,9 @@
+#ifndef MOUNT_HEADER_
+#define MOUNT_HEADER_
+
+#include <stdbool.h>
+
+bool mount_mount_filesystems(void);
+void mount_umount_filesystems(void);
+
+#endif

commit 27714f228359ddb55bddce9e59e4eb63374171ad

main: Prevent accidental closing of file descriptors
Methods `setup_stdio()` and `setup_stty()`, which run on children
process before `run_exec()` (and consequent change of process text) set
up children default file descriptors, `stdin`, `stderr` and `stdout`.

In this process, they use `dup2()` to set a preexistent file descriptor
to the default file descriptor - for instance, `setup_stdio()` dups log
fd on stderr and stdout. However, it may be possible that a preexistent
file descriptor has a number smaller or equal that of stderr. In this
case, `dup2()` will end up closing the preexistent file descriptor -
which may be undesirable, as it is when dups (and closes) log fd itself.

To prevent this, this patches introduces `safe_dup`, which must be
called to duplicate preexistent file descriptors to a number > stderr.
So, callers of `dup2()` can save any important file descriptor before
setting up default file descriptors.

Fixes: e01e5c55ff4bbbf4e175b3694055fd5c9b17be6b
Fixes: d69908a95df6d6bbebe7c61885a16643dd0bddd0

Reviewed-by: Flavio Ceolin <flavio.ceolin@intel.com>

src/main.c
@@ -128,6 +128,35 @@ static void run_exec(struct cmdline_contents *cmd_contents)
 	}
 }
 
+/*
+ * This function duplicates fd on a file descriptor whose number
+ * is assured to be bigger than STDERR_FILENO. This is necessary
+ * when init sets children default file descriptors (stdin, stdout,
+ * stderr) to avoid any useful file descriptor (like log_fd()) being
+ * accidentally closed because it had a number <= STDERR_FILENO.
+ */
+static bool safe_dup(int *fd)
+{
+	bool result = true;
+	int tmpfd;
+
+	errno = 0;
+	if (*fd <= STDERR_FILENO) {
+		tmpfd = fcntl(*fd, F_DUPFD, STDERR_FILENO + 1);
+
+		if (tmpfd < 0) {
+			log_message("Couldn't safe dup file descriptor\n");
+			result = false;
+			goto end;
+		}
+		(void)close(*fd);
+		*fd = tmpfd;
+	}
+
+end:
+	return result;
+}
+
 static void setup_signals(sigset_t *mask)
 {
 	int i, r;
@@ -246,6 +275,10 @@ static bool setup_stty(const char *terminal)
 		goto err_open;
 	}
 
+	if (!safe_dup(&tty)) {
+		goto err_dup_in;
+	}
+
 	r = dup2(tty, STDIN_FILENO);
 	if (r == -1) {
 		log_message("Could not dup terminal for STDIN: %m\n");
@@ -295,15 +328,23 @@ static bool setup_stdio(void)
 		goto err_open_null;
 	}
 
-	if (dup2(null_fd, STDIN_FILENO) == -1) {
-		log_message("Could not dup null fd: %m\n");
-		goto err_dup_null;
+	if (!safe_dup(&null_fd)) {
+		goto err_safe_dup_null;
 	}
 
 	out_fd = log_fd();
 	if (out_fd == -1) {
 		log_message("Could not open logfile: %m\n");
 		goto err_open_out;
+	}
+
+	if (!safe_dup(&out_fd)) {
+		goto err_safe_dup_out;
+	}
+
+	if (dup2(null_fd, STDIN_FILENO) == -1) {
+		log_message("Could not dup null fd: %m\n");
+		goto err_dup_null;
 	}
 
 	if (dup2(out_fd, STDOUT_FILENO) == -1) {
@@ -322,12 +363,14 @@ static bool setup_stdio(void)
 	return true;
 
 err_dup_err:
-	close(STDERR_FILENO);
+	close(STDOUT_FILENO);
 err_dup_out:
-	close(out_fd);
-err_open_out:
 	close(STDIN_FILENO);
 err_dup_null:
+err_safe_dup_out:
+	close(out_fd);
+err_open_out:
+err_safe_dup_null:
 	close(null_fd);
 err_open_null:
 	return false;