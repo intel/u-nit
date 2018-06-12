intel/u-nit

commit bce590ed9dad9a2c70f50c4a9dabbb0e8079e555

inittab: Move code related to semantics of inittab reading to own file
This should avoid cluttering main with this specialised code - main
should primarily handle process lifecycle.

Also reduced `parser` to a `lexer`: it only reads file and return
tokens. All semantic regarding inittab is on inittab.c. This makes more
natural the usage of a single `struct inittab_entry`.

Added `struct inittab` to aggregate expected types of entries: startup,
shutdown and safe-mode.

Reviewed-by: Flavio Ceolin <flavio.ceolin@intel.com>

Makefile
@@ -13,10 +13,11 @@ endif
 ALL: init
 
 SOURCE = \
+	src/inittab.c \
+	src/lexer.c \
 	src/log.c \
 	src/main.c \
-	src/mainloop.c \
-	src/parser.c
+	src/mainloop.c
 
 OBJS = $(SOURCE:.c=.o)
 

src/inittab.c
@@ -1,0 +1,348 @@
+#include "inittab.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include "log.h"
+#include "lexer.h"
+
+enum inittab_parse_result {RESULT_OK, RESULT_ERROR, RESULT_DONE};
+
+static bool
+safe_strtoi32_t(const char *s, int32_t *ret)
+{
+    char *end_ptr = NULL;
+    long int l;
+    bool result = true;
+
+    errno = 0;
+    l = strtol(s, &end_ptr, 10);
+
+    if (((errno != 0) || (*end_ptr != '\0')) || ((l > INT32_MAX) || (l < INT32_MIN))) {
+        result = false;
+    }
+
+    if (result) {
+        *ret = (int32_t)l;
+    }
+
+    return result;
+}
+
+static void
+add_entry_to_list(struct inittab_entry **list, struct inittab_entry *entry)
+{
+    struct inittab_entry *current;
+
+    assert(list != NULL);
+    assert(entry != NULL);
+
+    /* Add item to list ensuring entry->order is respected,
+     * and keeping list stable - or, if a new item with same order
+     * is added, it is kept after all previous items */
+    if ((*list == NULL) || ((*list)->order > entry->order)) {
+        entry->next = *list;
+        *list = entry;
+    } else {
+        current = *list;
+        while ((current->next != NULL) && (current->next->order <= entry->order)) {
+            current = current->next;
+        }
+        entry->next = current->next;
+        current->next = entry;
+    }
+}
+
+static void
+debug_inittab_entry_list(struct inittab_entry *list)
+{
+    if (list == NULL) {
+        log_message("\tNULL\n");
+    } else {
+        struct inittab_entry *current = list;
+
+        while (current != NULL) {
+            log_message("\t[Entry] order: %d, core_id: %d, type: %d, process: '%s'\n",
+                    current->order, current->core_id, current->type, current->process_name);
+            current = current->next;
+        }
+    }
+}
+
+static void
+debug_inittab_entries(struct inittab *inittab_entries)
+{
+    log_message("STARTUP LIST:\n");
+    debug_inittab_entry_list(inittab_entries->startup_list);
+
+    log_message("SHUTDOWN LIST:\n");
+    debug_inittab_entry_list(inittab_entries->shutdown_list);
+
+    log_message("SAFE MODE:\n");
+    debug_inittab_entry_list(inittab_entries->safe_mode_entry);
+}
+
+void
+free_inittab_entry_list(struct inittab_entry *list)
+{
+    struct inittab_entry *tmp;
+
+    while (list != NULL) {
+        tmp = list;
+        list = list->next;
+        free(tmp);
+    }
+}
+
+static bool
+place_entry(struct inittab_entry *entry, struct inittab *inittab_entries)
+{
+    bool result = true;
+
+    assert(entry != NULL);
+
+    switch (entry->type) {
+        case ONE_SHOT:
+        case SAFE_ONE_SHOT:
+        case SERVICE:
+        case SAFE_SERVICE: {
+            add_entry_to_list(&inittab_entries->startup_list, entry);
+            break;
+        }
+        case SHUTDOWN:
+        case SAFE_SHUTDOWN: {
+            add_entry_to_list(&inittab_entries->shutdown_list, entry);
+            break;
+        }
+        case SAFE_MODE: {
+            if (inittab_entries->safe_mode_entry != NULL) {
+                log_message("Safe process already defined before '%.20s'(...)\n", entry->process_name);
+                result = false;
+                break;
+            }
+            add_entry_to_list(&inittab_entries->safe_mode_entry, entry);
+            break;
+        }
+        default: {
+            /* Should never happen */
+            assert(false);
+            break;
+        }
+    }
+
+    return result;
+}
+
+static enum inittab_parse_result
+inittab_parse_entry(FILE *fp, struct inittab_entry *entry)
+{
+    char buf[BUFFER_LEN] = { };
+    struct lexer_data lexer = { };
+    enum inittab_parse_result result = RESULT_OK;
+    enum next_line_result next;
+    enum token_result tr;
+
+    char *order_str = NULL, *core_id_str = NULL, *type_str = NULL, *process_str = NULL;
+
+    if ((fp == NULL) || (feof(fp) != 0)) {
+        result = RESULT_ERROR;
+        goto end;
+    }
+
+    next = inittab_next_line(fp, buf);
+
+    if (next == NEXT_LINE_TOO_BIG) {
+        log_message("Line too big: '%.20s(...)'\n", buf);
+        result = RESULT_ERROR;
+        goto end;
+    } else if (next == NEXT_LINE_ERROR) {
+        log_message("Couldn't read inittab file\n");
+        result = RESULT_ERROR;
+        goto end;
+    } else if (next == NEXT_LINE_EOF){
+        result = RESULT_DONE;
+        goto end;
+    } else {
+        /* Everything is fine */
+    }
+
+    /* Set lexer up */
+    init_lexer(&lexer, buf, sizeof(buf));
+
+    /* Get <order> */
+    tr = next_token(&lexer, &order_str, ':');
+    if (tr == TOKEN_BLANK) {
+        entry->order = -1;
+    } else if (tr == TOKEN_ERROR) {
+        log_message("Invalid 'order' field on inittab entry\n");
+        result = RESULT_ERROR;
+        goto end;
+    } else {
+        if (!safe_strtoi32_t(order_str, &entry->order)) {
+            log_message("Invalid 'order' field on inittab entry\n");
+            result = RESULT_ERROR;
+            goto end;
+        }
+        if (entry->order < 0) {
+            log_message("Invalid order 'field' on inittab entry\n");
+            result = RESULT_ERROR;
+            goto end;
+        }
+    }
+
+    /* Get <core_id> */
+    tr = next_token(&lexer, &core_id_str, ':');
+    if (tr == TOKEN_BLANK) {
+        entry->core_id = -1;
+    } else if (tr == TOKEN_ERROR) {
+        log_message("Invalid 'core_id' field on inittab entry\n");
+        result = RESULT_ERROR;
+        goto end;
+    } else {
+        if (!safe_strtoi32_t(core_id_str, &entry->core_id)) {
+            log_message("Invalid 'core_id' field on inittab entry\n");
+            result = RESULT_ERROR;
+            goto end;
+        }
+        if (entry->core_id < 0) {
+            log_message("Invalid 'core_id' field on inittab entry\n");
+            result = RESULT_ERROR;
+            goto end;
+        }
+    }
+
+    /*Get <type> */
+    tr = next_token(&lexer, &type_str, ':');
+    if (tr != TOKEN_OK) {
+        log_message("Expected 'type' field on inittab entry\n");
+        result = RESULT_ERROR;
+        goto end;
+    } else {
+        if (strcmp(type_str, "<one-shot>") == 0) {
+            entry->type = ONE_SHOT;
+        } else if (strcmp(type_str, "<safe-one-shot>") == 0) {
+            entry->type = SAFE_ONE_SHOT;
+        } else if (strcmp(type_str, "<service>") == 0) {
+            entry->type = SERVICE;
+        } else if (strcmp(type_str, "<safe-service>") == 0) {
+            entry->type = SAFE_SERVICE;
+        } else if (strcmp(type_str, "<shutdown>") == 0) {
+            entry->type = SHUTDOWN;
+        } else if (strcmp(type_str, "<safe-shutdown>") == 0) {
+            entry->type = SAFE_SHUTDOWN;
+        } else if (strcmp(type_str, "<safe-mode>") == 0) {
+            entry->type = SAFE_MODE;
+        } else {
+            log_message("Invalid 'type' field on inittab entry: %s\n", type_str);
+            result = RESULT_ERROR;
+            goto end;
+        }
+    }
+
+    /* Now that we know entry type, check if it has a valid order */
+    if ((entry->order == -1) && (entry->type != SAFE_MODE)) {
+        log_message("Expected 'order' field on entry with type different of '<safe-mode>'\n");
+        result = RESULT_ERROR;
+        goto end;
+    }
+
+    /*Get <process> */
+    tr = next_token(&lexer, &process_str, '\0');
+    if (tr != TOKEN_OK) {
+        log_message("Expected 'process' field on inittab entry\n");
+        result = RESULT_ERROR;
+        goto end;
+    } else if (strlen(process_str) < sizeof(entry->process_name)){
+        (void)strcpy(entry->process_name, process_str);
+    } else {
+        log_message("Invalid 'process' field on inittab entry\n");
+        result = RESULT_ERROR;
+        goto end;
+    }
+
+end:
+    return result;
+}
+
+bool
+read_inittab(const char *filename, struct inittab *inittab_entries)
+{
+    FILE *fp = NULL;
+    struct inittab_entry *entry;
+    enum inittab_parse_result r;
+    bool error = false, result = true;
+
+    assert(filename != NULL);
+    assert(inittab_entries != NULL);
+
+    errno = 0;
+    fp = fopen(filename, "re");
+    if (fp == NULL) {
+        log_message("Couldn't open inittab file: %m\n");
+        result = false;
+        goto end;
+    }
+
+    log_message("Reading inittab entries...\n");
+    while (true) {
+        bool exit_loop = false;
+
+        entry = calloc(1, sizeof(struct inittab_entry));
+        if (entry == NULL) {
+            log_message("Could not allocate memory to parse inittab entry\n");
+            error = true;
+            break;
+        }
+
+        r = inittab_parse_entry(fp, entry);
+        if (r == RESULT_OK) {
+            log_message("[Entry] order: %d, core_id: %d, type: %d, process: '%s'\n",
+                    entry->order, entry->core_id, entry->type, entry->process_name);
+
+            if (!place_entry(entry, inittab_entries)) {
+                free(entry);
+                error = true;
+                exit_loop = true;
+            }
+        } else if (r == RESULT_ERROR) {
+            error = true;
+            free(entry);
+            /* TODO currently, `inittab_parse_entry` itself prints error. Maybe it'd
+             * better if it returned (via a pointer arg) information about the error,
+             * so caller print it */
+        } else {
+            exit_loop = true;
+            free(entry);
+        }
+
+        if (exit_loop) {
+            break;
+        }
+    }
+
+    if (inittab_entries->safe_mode_entry == NULL) {
+        log_message("No <safe-mode> entry on inittab. Can't go on!\n");
+        error = true;
+        /* TODO is this the right approach? */
+    }
+
+    if (error) {
+        log_message("Error(s) during inittab parsing. Exiting!\n");
+        result = false;
+
+        free_inittab_entry_list(inittab_entries->startup_list);
+        free_inittab_entry_list(inittab_entries->shutdown_list);
+        free_inittab_entry_list(inittab_entries->safe_mode_entry);
+    } else {
+        debug_inittab_entries(inittab_entries);
+    }
+
+    errno = 0;
+    if (fclose(fp) != 0) {
+        log_message("Error closing inittab file: %m\n");
+    }
+
+end:
+    return result;
+}

src/inittab.h
@@ -1,23 +1,29 @@
-#ifndef PARSER_HEADER_
-#define PARSER_HEADER_
+#ifndef INITTAB_HEADER_
+#define INITTAB_HEADER_
 
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 
 enum inittab_entry_type {ONE_SHOT, SAFE_ONE_SHOT, SERVICE, SAFE_SERVICE,
     SHUTDOWN, SAFE_SHUTDOWN, SAFE_MODE};
-enum inittab_parse_result {RESULT_OK, RESULT_ERROR, RESULT_DONE};
 
 struct inittab_entry {
+    struct inittab_entry *next;
     char process_name[4096];
     int32_t order;
     int32_t core_id;
     enum inittab_entry_type type;
 };
 
-enum inittab_parse_result
-inittab_parse_entry(FILE *fp, struct inittab_entry *entry);
+struct inittab {
+    struct inittab_entry *startup_list;
+    struct inittab_entry *shutdown_list;
+    struct inittab_entry *safe_mode_entry;
+};
+
+bool read_inittab(const char *filename, struct inittab *inittab_entries);
+void free_inittab_entry_list(struct inittab_entry *list);
 
 static inline bool
 is_safe_entry(const struct inittab_entry *entry)
@@ -64,4 +70,5 @@ is_one_shot_entry(const struct inittab_entry *entry)
         (entry->type == SHUTDOWN) ||
         (entry->type == SAFE_SHUTDOWN);
 }
+
 #endif

src/lexer.c
@@ -1,0 +1,109 @@
+#include "lexer.h"
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "log.h"
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+void
+init_lexer(struct lexer_data *lexer, char *buf, size_t size)
+{
+    lexer->buf = buf;
+    lexer->size = size;
+    lexer->pos = 0U;
+};
+
+enum token_result
+next_token(struct lexer_data *lexer, char **token, char delim)
+{
+    size_t start_pos = lexer->pos;
+    enum token_result ret;
+
+    while ((lexer->buf[lexer->pos] != delim) && (lexer->buf[lexer->pos] != '\0') && (lexer->pos < lexer->size)) {
+        lexer->pos++;
+    }
+
+    if (lexer->pos == start_pos) {
+        /* No content on token before next delimiter, so it's a blank one */
+        ret = TOKEN_BLANK;
+    } else if (lexer->pos >= lexer->size) {
+        /* No token and buffer finished - raise error */
+        ret = TOKEN_ERROR;
+    } else {
+        /* Delimiter or '\0' was found. Happiness */
+        ret = TOKEN_OK;
+    }
+
+    if (ret != TOKEN_ERROR) {
+        /* Adjust delimiter to '\0', so token returned ends properly */
+        lexer->buf[lexer->pos] = '\0';
+        *token = &lexer->buf[start_pos];
+
+        /* Advance lexer current position so next token starts properly */
+        lexer->pos++;
+    }
+
+    return ret;
+}
+
+static void
+discard_line(FILE *fp)
+{
+    char buf[BUFFER_LEN];
+
+    while (true) {
+        int c;
+
+        (void)fscanf(fp, "%"STR(LINE_SIZE)"[^\n]", buf);
+        c = fgetc(fp); /* Discards any left '\n' */
+
+        if ((c == (int)'\n') || (c == EOF)) {
+            break;
+        }
+    }
+}
+
+enum next_line_result
+inittab_next_line(FILE *fp, char buf[])
+{
+    int r = 0, c = 0;
+    enum next_line_result result = NEXT_LINE_ERROR;
+
+    while (true) {
+        bool end_loop = false;
+
+        r = fscanf(fp, "%"STR(LINE_SIZE)"[^\n]", buf);
+        c = fgetc(fp); /* Discards any left '\n' */
+
+        if (r == EOF) {
+            end_loop = true; /* Read error or nothing more to read */
+            if (feof(fp) != 0) {
+                result = NEXT_LINE_EOF;
+            } else {
+                result = NEXT_LINE_ERROR;
+            }
+        } else if ((buf[0] == '#') && ((c == (int)'\n') || (c == EOF))) {
+            /* Read a small comment, move to next line*/
+        } else if (buf[0] == '\0') {
+            /* Read empty line, move to next line*/
+        } else if ((c != (int)'\n') && (c != EOF)) {
+            end_loop = true; /* Line too big */
+            result = NEXT_LINE_TOO_BIG;
+            discard_line(fp);
+        } else {
+            end_loop = true;
+            result = NEXT_LINE_OK;
+        }
+
+        if (end_loop) {
+            break;
+        }
+    }
+
+    return result;
+}

src/lexer.h
@@ -1,0 +1,24 @@
+#ifndef LEXER_HEADER_
+#define LEXER_HEADER_
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define LINE_SIZE 4095
+#define BUFFER_LEN LINE_SIZE + 1
+
+enum token_result {TOKEN_OK, TOKEN_BLANK, TOKEN_ERROR};
+enum next_line_result {NEXT_LINE_OK, NEXT_LINE_TOO_BIG, NEXT_LINE_EOF, NEXT_LINE_ERROR};
+
+struct lexer_data {
+    char *buf;
+    size_t size;
+    size_t pos;
+};
+
+void init_lexer(struct lexer_data *lexer, char *buf, size_t size);
+enum next_line_result inittab_next_line(FILE *fp, char buf[]);
+enum token_result next_token(struct lexer_data *lexer, char **token, char delim);
+
+#endif

src/main.c
@@ -11,9 +11,9 @@
 #include <sys/signalfd.h>
 #include <unistd.h>
 
+#include "inittab.h"
 #include "log.h"
 #include "mainloop.h"
-#include "parser.h"
 
 #ifndef TIMEOUT_TERM
 #define TIMEOUT_TERM 3000
@@ -38,21 +38,14 @@ struct process {
     pid_t pid;
 };
 
-struct inittab_entry_list {
-    struct inittab_entry_list *next;
-    struct inittab_entry entry;
-};
-
 struct remaining_entries {
-    struct inittab_entry_list *remaining;
+    struct inittab_entry *remaining;
     uint32_t pending_finish;
 };
 
 static struct remaining_entries remaining;
 
-static struct inittab_entry safe_mode_entry;
-static struct inittab_entry_list *startup_entries;
-static struct inittab_entry_list *shutdown_entries;
+static struct inittab inittab_entries;
 
 static struct process *running_processes;
 
@@ -69,88 +62,6 @@ usage(const char *invocation_name)
             "\n"
             "System and service manager [early development]\n",
             invocation_name);
-}
-
-static bool
-add_entry_to_list(struct inittab_entry_list **list, struct inittab_entry *entry)
-{
-    struct inittab_entry_list *current;
-    struct inittab_entry_list *list_item;
-    bool result = true;
-
-    assert(list != NULL);
-    assert(entry != NULL);
-
-    list_item = calloc(1, sizeof(struct inittab_entry_list));
-    if (list_item == NULL) {
-        result = false;
-        goto end;
-    }
-
-    memcpy(&list_item->entry, entry, sizeof(struct inittab_entry));
-
-    /* Add item to list ensuring item->config.order is respected,
-     * and keeping list stable - or, if a new item with same order
-     * is added, it is kept after all previous items */
-    if ((*list == NULL) || ((*list)->entry.order > entry->order)) {
-        list_item->next = *list;
-        *list = list_item;
-    } else {
-        current = *list;
-        while ((current->next != NULL) && (current->next->entry.order <= entry->order)) {
-            current = current->next;
-        }
-        list_item->next = current->next;
-        current->next = list_item;
-    }
-
-end:
-    return result;
-}
-
-static void
-debug_entries_lists(void)
-{
-    struct inittab_entry_list *entry;
-
-    log_message("STARTUP LIST:\n");
-    entry = startup_entries;
-    while (entry != NULL) {
-        log_message("\t[Entry] order: %d, core_id: %d, type: %d, process: '%s'\n",
-                entry->entry.order, entry->entry.core_id, entry->entry.type, entry->entry.process_name);
-        entry = entry->next;
-    }
-
-    log_message("SHUTDOWN LIST:\n");
-    entry = shutdown_entries;
-    while (entry != NULL) {
-        log_message("\t[Entry] order: %d, core_id: %d, type: %d, process: '%s'\n",
-                entry->entry.order, entry->entry.core_id, entry->entry.type, entry->entry.process_name);
-        entry = entry->next;
-    }
-
-    log_message("SAFE MODE:\n");
-    log_message("\t[Entry] order: %d, core_id: %d, type: %d, entry: '%s'\n",
-            safe_mode_entry.order, safe_mode_entry.core_id,
-            safe_mode_entry.type, safe_mode_entry.process_name);
-}
-
-static void
-free_entries_lists(void)
-{
-    struct inittab_entry_list *tmp;
-
-    while (startup_entries != NULL) {
-        tmp = startup_entries;
-        startup_entries = startup_entries->next;
-        free(tmp);
-    }
-
-    while (shutdown_entries != NULL) {
-        tmp = shutdown_entries;
-        shutdown_entries = shutdown_entries->next;
-        free(tmp);
-    }
 }
 
 static void
@@ -188,45 +99,6 @@ free_process_list(struct process **list)
     while (*list != NULL) {
         remove_process(list, *list);
     }
-}
-
-static bool
-handle_entry(struct inittab_entry *entry)
-{
-    bool result = true;
-
-    assert(entry != NULL);
-
-    switch (entry->type) {
-        case ONE_SHOT:
-        case SAFE_ONE_SHOT:
-        case SERVICE:
-        case SAFE_SERVICE: {
-            add_entry_to_list(&startup_entries, entry);
-            break;
-        }
-        case SHUTDOWN:
-        case SAFE_SHUTDOWN: {
-            add_entry_to_list(&shutdown_entries, entry);
-            break;
-        }
-        case SAFE_MODE: {
-            log_message(">>>>> [%s]\n", safe_mode_entry.process_name);
-            if (safe_mode_entry.process_name[0] != '\0') {
-                log_message("Safe process already defined before '%.20s'(...)\n", entry->process_name);
-                result = false;
-                break;
-            }
-            memcpy(&safe_mode_entry, entry, sizeof(struct inittab_entry));
-            break;
-        }
-        default: {
-            /* Should never happen */
-            assert(false);
-        }
-    }
-
-    return result;
 }
 
 static int
@@ -321,18 +193,19 @@ one_shot_timeout_cb(void)
 }
 
 static bool
-start_processes(struct inittab_entry_list *list)
+start_processes(struct inittab_entry *list)
 {
     int32_t current_order;
     bool result = true;
 
     if (list != NULL) {
-        struct inittab_entry_list *list_item;
+        struct inittab_entry *entry;
 
         remaining.pending_finish = 0;
-        current_order = list->entry.order;
-        list_item = list;
-        while ((list_item != NULL) && (list_item->entry.order == current_order)) {
+        entry = list;
+        current_order = entry->order;
+
+        while ((entry != NULL) && (entry->order == current_order)) {
             struct process *p;
 
             /* First, let's see if we have memory for anciliary struct */
@@ -341,11 +214,11 @@ start_processes(struct inittab_entry_list *list)
                 result = false;
                 break;
             }
-            p->pid = spawn_exec(list_item->entry.process_name, "");
+            p->pid = spawn_exec(entry->process_name, "");
 
             if (p->pid > 0) {
                 /* Stores inittab entry information on process struct */
-                memcpy(&p->config, &(list_item->entry), sizeof(struct inittab_entry));
+                memcpy(&p->config, entry, sizeof(struct inittab_entry));
 
                 if (is_one_shot_entry(&p->config)) {
                     remaining.pending_finish++;
@@ -359,10 +232,10 @@ start_processes(struct inittab_entry_list *list)
                 /* TODO what to do? if a safe one fails, maybe safe state?*/
             }
 
-            list_item = list_item->next;
+            entry = entry->next;
         }
 
-        remaining.remaining = list_item;
+        remaining.remaining = entry;
     }
 
     if (result) {
@@ -374,75 +247,6 @@ start_processes(struct inittab_entry_list *list)
         }
     }
 
-    return result;
-}
-
-static bool
-read_inittab(const char *filename)
-{
-    FILE *fp = NULL;
-    struct inittab_entry entry = {0};
-    enum inittab_parse_result r;
-    bool error = false, result = true;
-
-    assert(filename != NULL);
-
-    errno = 0;
-    fp = fopen(filename, "re");
-    if (fp == NULL) {
-        log_message("Couldn't open inittab file: %m\n");
-        result = false;
-        goto end;
-    }
-
-    log_message("Reading inittab entries...\n");
-    while (true) {
-        bool exit_loop = false;
-
-        r = inittab_parse_entry(fp, &entry);
-        if (r == RESULT_OK) {
-            log_message("[Entry] order: %d, core_id: %d, type: %d, process: '%s'\n",
-                    entry.order, entry.core_id, entry.type, entry.process_name);
-
-            if (!handle_entry(&entry)) {
-                error = true;
-                exit_loop = true;
-            }
-        } else if (r == RESULT_ERROR) {
-            error = true;
-            /* TODO currently, `inittab_parse_entry` itself prints error. Maybe it's
-             * better if it returned (via a pointer arg) information about the error,
-             * so caller print it */
-        } else {
-            exit_loop = true;
-        }
-
-        if (exit_loop) {
-            break;
-        }
-    }
-
-    if (safe_mode_entry.process_name[0] == '\0') {
-        log_message("No <safe-mode> entry on inittab. Can't go on!\n");
-        error = true;
-        /* TODO is this the right approach? */
-    }
-
-    if (error) {
-        log_message("Error(s) during inittab parsing. Exiting!\n");
-        result = false;
-        free_entries_lists();
-
-        goto cleanup;
-    }
-
-cleanup:
-    errno = 0;
-    if (fclose(fp) != 0) {
-        log_message("Error closing inittab file: %m\n");
-    }
-
-end:
     return result;
 }
 
@@ -479,7 +283,7 @@ stage_maintenance(void)
         /* If all process finished, time to start 'shutdown' ones */
         if (running_processes == NULL) {
             current_stage = STAGE_SHUTDOWN;
-            start_processes(shutdown_entries);
+            start_processes(inittab_entries.shutdown_list);
 
             /* Since all processes ended, no need for timer to kill them anymore */
             if (kill_timeout != NULL) {
@@ -630,12 +434,10 @@ main(int argc, char *argv[])
         goto end;
     }
 
-    if (!read_inittab(argv[1])) {
+    if (!read_inittab(argv[1], &inittab_entries)) {
         result = EXIT_FAILURE;
         goto end;
     }
-
-    debug_entries_lists();
 
     /* Block signals that should only be caught by epoll */
     setup_signals(&mask);
@@ -656,14 +458,17 @@ main(int argc, char *argv[])
 
     /* Start initial list of process */
     current_stage = STAGE_STARTUP;
-    start_processes(startup_entries);
+    start_processes(inittab_entries.startup_list);
 
     mainloop_start();
 
 end:
 
-    free_entries_lists();
     free_process_list(&running_processes);
+
+    free_inittab_entry_list(inittab_entries.startup_list);
+    free_inittab_entry_list(inittab_entries.shutdown_list);
+    free_inittab_entry_list(inittab_entries.safe_mode_entry);
 
     if (msh != NULL) {
         mainloop_remove_signal_handler(msh);

commit fcc969e126ff15de441310f450cf255a09c54777

tests: Bring parser_test.c to life again
It stopped working since "inittab: Move code related to semantics(...)"
commit. Now it is a white-box test, testing internals of inittab.c.

Fixes: bce590ed9dad9a2c70f50c4a9dabbb0e8079e555

Reviewed-by: Flavio Ceolin <flavio.ceolin@intel.com>

Makefile
@@ -34,7 +34,7 @@ clean:
 
 TESTS = parser_test
 
-parser_test: src/parser.o src/log.o tests/parser_test.c
+parser_test: src/lexer.o src/log.o tests/parser_test.c
 	$(CC) $(TESTS_CFLAGS) $^ -o $@ $(LDFLAGS)
 
 tests: $(TESTS)

tests/parser_test.c
@@ -1,10 +1,16 @@
-#include <parser.h>
-
 #include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+
+/*
+ * This is a whitebox testing of an internal function of `inittab.c`
+ * (inittab_parse_entry).
+ * That's why we include it directly here. This whitebox test is useful
+ * because allows a more granular (line by line) checking of parser.
+ */
+#include <inittab.c>
 
 struct test_data {
     const char *file_name;